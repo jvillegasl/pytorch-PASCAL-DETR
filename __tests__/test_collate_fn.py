3265d2be30d5485796b369a4cb46afe29553a2ff
jvillegasl <jvillegasl@uni.pe>
2023-06-25 21:33:54 -0500

    fix: normalized output test implemented

--- __tests__/test_collate_fn.py
+++ __tests__/test_collate_fn.py
@@ -1,7 +1,7 @@
 import torch
+import random
 
 import inject_modules
-# from data_loader.data_loaders import PascalDataLoader
 from data_loader.collate_fn import collate_fn, annotation_to_tensor, object_to_tensor
 from mock_data import CLASSES, mock_object, mock_dataset, mock_annotation, mock_image
 
@@ -17,10 +17,11 @@ class TestCollateFn:
 
     @classmethod
     def setup_class(cls):
-        cls.dataset = mock_dataset(cls.batch_size, cls.chw, cls.max_num_objects)
+        cls.dataset = mock_dataset(
+            cls.batch_size, cls.chw, cls.max_num_objects)
 
     def test_object_to_tensor(self):
-        H, W = 500, 500
+        H, W = random.randint(0, 1000), random.randint(0, 1000)
 
         obj = mock_object(H, W)
         tensor = object_to_tensor(obj, self.classes, H, W)
@@ -34,9 +35,13 @@ class TestCollateFn:
 
         assert list(class_index.shape) == [1]
         assert list(bbox.shape) == [4]
+        assert torch.all(torch.logical_and(
+            bbox >= 0,
+            bbox <= 1
+        ))
 
     def test_annotation_to_tensor(self):
-        C, H, W = self.chw
+        C, H, W = 3, random.randint(0, 1000), random.randint(0, 1000)
 
         image = mock_image(C, H, W)
         annotation = mock_annotation(image, self.max_num_objects)['annotation']
@@ -54,9 +59,13 @@ class TestCollateFn:
 
         assert list(classes.shape) == [self.max_num_objects, 1]
         assert list(bboxes.shape) == [self.max_num_objects, 4]
+        assert torch.all(torch.logical_and(
+            bboxes >= 0,
+            bboxes <= 1
+        ))
 
     def test_collate_fn(self):
-        batch = collate_fn(self.dataset, self.classes, self.max_num_objects)
+        batch = collate_fn(self.dataset, self.classes)
 
         xb, yb = batch
 
@@ -72,7 +81,9 @@ class TestCollateFn:
 
         yb_classes, yb_bboxes = yb[0], yb[1]
 
-        assert list(yb_classes.shape) == [
-            self.batch_size, self.max_num_objects, 1]
-        assert list(yb_bboxes.shape) == [
-            self.batch_size, self.max_num_objects, 4]
+        assert yb_classes.size(0) == self.batch_size and yb_classes.size(2) == 1
+        assert yb_bboxes.size(0) == self.batch_size and yb_bboxes.size(2)  == 4
+        assert torch.all(torch.logical_and(
+            yb_bboxes >= 0,
+            yb_bboxes <= 1
+        ))
