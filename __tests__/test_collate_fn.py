a6c2eb75a39d18111e9c7e44d94de9c1204dba34
jvillegasl <jvillegasl@uni.pe>
2023-06-27 23:30:35 -0500

    update: collate test adapted to new output format

--- __tests__/test_collate_fn.py
+++ __tests__/test_collate_fn.py
@@ -2,88 +2,88 @@ import torch
 import random
 
 import inject_modules
-from data_loader.collate_fn import collate_fn, annotation_to_tensor, object_to_tensor
-from mock_data import CLASSES, mock_object, mock_dataset, mock_annotation, mock_image
+from data_loader.collate_fn import collate_fn, annotation_to_batch_item
+from mock_data import CLASSES, mock_dataset, mock_annotation, mock_image
+from utils.check_exception import check_exception
 
 
 class TestCollateFn:
-    dataset: list[list]
-    batch_size = 64
+    def test_annotation_to_batch_item(self):
+        C = 3
+        H = random.randint(100, 1000)
+        W = random.randint(100, 1000)
+        max_num_objects = random.randint(10, 100)
+        classes = CLASSES
 
-    chw = (3, 224, 224)
-
-    classes = CLASSES
-    max_num_objects = 100
-
-    @classmethod
-    def setup_class(cls):
-        cls.dataset = mock_dataset(
-            cls.batch_size, cls.chw, cls.max_num_objects)
-
-    def test_object_to_tensor(self):
-        H, W = random.randint(0, 1000), random.randint(0, 1000)
-
-        obj = mock_object(H, W)
-        tensor = object_to_tensor(obj, self.classes, H, W)
-
-        assert isinstance(tensor, list)
-        assert len(tensor) == 2
-
-        class_index, bbox = tensor
-        assert isinstance(class_index, torch.Tensor)
-        assert isinstance(bbox, torch.Tensor)
-
-        assert list(class_index.shape) == [1]
-        assert list(bbox.shape) == [4]
-        assert torch.all(torch.logical_and(
-            bbox >= 0,
-            bbox <= 1
-        ))
-
-    def test_annotation_to_tensor(self):
-        C, H, W = 3, random.randint(0, 1000), random.randint(0, 1000)
+        num_objects = random.randint(1, max_num_objects)
 
         image = mock_image(C, H, W)
-        annotation = mock_annotation(image, self.max_num_objects)['annotation']
+        annotation = mock_annotation(image, num_objects)
 
-        out = annotation_to_tensor(
-            annotation, self.classes, self.max_num_objects)
+        x = annotation_to_batch_item(annotation, classes)
 
-        assert isinstance(out, list)
-        assert len(out) == 2
-        assert isinstance(out[0], torch.Tensor)
-        assert isinstance(out[1], torch.Tensor)
+        x = self.assert_annotation_batch_item(x)
 
-        classes = out[0]
-        bboxes = out[1]
+        labels_ids = x['labels']
+        bboxes = x['bboxes']
 
-        assert list(classes.shape) == [self.max_num_objects, 1]
-        assert list(bboxes.shape) == [self.max_num_objects, 4]
+        assert labels_ids.shape == torch.Size([num_objects])
+        assert bboxes.shape == torch.Size([num_objects, 4])
+
+    def test_collate_fn(self):
+        batch_size = random.randint(1, 100)
+        C = 3
+        H = random.randint(100, 1000)
+        W = random.randint(100, 1000)
+        max_num_objects = random.randint(10, 100)
+        classes = CLASSES
+
+        dataset_batch = mock_dataset(batch_size, (C, H, W), max_num_objects)
+
+        batch = collate_fn(dataset_batch, classes)
+
+        assert isinstance(batch, tuple)
+        assert len(batch) == 2
+
+        xb, yb = batch
+
+        assert isinstance(xb, torch.Tensor)
+        assert xb.shape == torch.Size((batch_size, C, H, W))
+
+        assert isinstance(yb, list)
+        assert len(yb) == batch_size
+        assert all(
+            [
+                all([
+                    isinstance(y, dict),
+                    'labels' in y and 'bboxes' in y,
+                    not check_exception(
+                        lambda: self.assert_annotation_batch_item(y),
+                        AssertionError
+                    )
+                ])
+                for y in yb
+            ]
+        )
+
+    def assert_annotation_batch_item(self, x):
+        assert isinstance(x, dict)
+        assert 'labels' in x and 'bboxes' in x
+
+        labels_ids = x['labels']
+        bboxes = x['bboxes']
+
+        assert isinstance(labels_ids, torch.Tensor)
+        assert len(labels_ids.shape) == 1
+        assert labels_ids.dtype == torch.int64
+
+        assert isinstance(bboxes, torch.Tensor)
+        assert len(bboxes.shape) == 2
+        assert bboxes.size(0) == labels_ids.size(0)
+        assert bboxes.size(1) == 4
         assert torch.all(torch.logical_and(
             bboxes >= 0,
             bboxes <= 1
         ))
 
-    def test_collate_fn(self):
-        batch = collate_fn(self.dataset, self.classes)
-
-        xb, yb = batch
-
-        assert isinstance(xb, torch.Tensor)
-
-        xb_shape = xb.shape
-        assert list(xb_shape) == [self.batch_size, *self.chw]
-
-        assert isinstance(yb, list)
-        assert len(yb) == 2
-        assert isinstance(yb[0], torch.Tensor) and isinstance(
-            yb[1], torch.Tensor)
-
-        yb_classes, yb_bboxes = yb[0], yb[1]
-
-        assert yb_classes.size(0) == self.batch_size and yb_classes.size(2) == 1
-        assert yb_bboxes.size(0) == self.batch_size and yb_bboxes.size(2)  == 4
-        assert torch.all(torch.logical_and(
-            yb_bboxes >= 0,
-            yb_bboxes <= 1
-        ))
+        return {'labels': labels_ids, 'bboxes': bboxes}
